e550ee6837f0d03d7a4053d53836df534a566d42
Jeimmi <jeimmi.gomez@gmail.com>
2016-03-07 22:23:42 -0800

    commiting changes

---
+++ app/src/main/java/com/example/jeimmi/weatherdemo/WeatherStaion.java
@@ -0,0 +1,13 @@
+package com.example.jeimmi.weatherdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WeatherStaion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
